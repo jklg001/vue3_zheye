79b830f83014a751daa7bc3b61a66f5649ee7b4a
lijiaming <947592159@qq.com>
2022-03-12 20:26:38 +0800

    者也论坛初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a class="navbar-brand" href="#">者也</a>
+   <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登陆</a></li>
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <drop-down :title="user.name"></drop-down>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import DropDown from './DropDown.vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  components: {
+    DropDown
+  },
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
